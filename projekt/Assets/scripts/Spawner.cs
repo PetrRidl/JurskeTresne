84dfeb033c119549db1b7acc58b64e4ebca88dd6
PetrRidl <72690578+PetrRidl@users.noreply.github.com>
2021-04-14 22:18:49 +0200

    Update

---
+++ projekt/Assets/scripts/Spawner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+
+public class Spawner : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.A))
+    }
+    private void Spawn(){
+       
+
+    
+    }
+    //Character nepritel = Character.Create(spawnPosition);
+
+}
