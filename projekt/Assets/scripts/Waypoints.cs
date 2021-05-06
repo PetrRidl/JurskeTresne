9f2b9c82c8a959c5ac2a3b60e779f87a048353fb
PetrRidl <72690578+PetrRidl@users.noreply.github.com>
2021-05-06 09:03:06 +0200

    Smazání staré verze projektu

--- projekt/Assets/scripts/Waypoints.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
-using UnityEngine;
-
-public class Waypoints : MonoBehaviour
-{
-    public Transform[] waypoints;
-}
